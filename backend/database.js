8ae14871f393f154e0c3b959a7c6995b6ce6a542
Rakshit <125498029+Rz4377@users.noreply.github.com>
2024-08-09 09:45:18 +0530

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
-mongoose.
-connect('mongodb+srv://amansharma323298:Uxsp7Lo90EB4tkq1@cluster0.d3vqt5e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect(''); // connect using moongoose url 
 
 const CourseSchema = new Schema ({
     title : String , 
